485ae77d9df6beab00d09c12579f75f439a9a9d8
seimon-m <simimueller@hotmail.com>
2021-04-01 13:28:53 +0200

    add tech stack description

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="test">
         <h1>HALLOO VELLOOOOO</h1>
-        <p>
-            For a guide and recipes on how to configure / customize this
-            project,<br />
-            check out the
-        </p>
+        <h3>Tech stack</h3>
+        <p>Framework: vue.js (Version 3) <a>https://v3.vuejs.org/guide/introduction.html</a></p>
+        <p>Animation Library: GSAP <a>https://greensock.com/get-started/</a></p>
+        <p>Contiuous Deployment: Netlify <a>https://stuemtec-focus.netlify.app/</a></p>
     </div>
 </template>
 
@@ -15,7 +14,7 @@ import { gsap } from "gsap";
 export default {
     name: "TestComponent",
     mounted() {
-        gsap.to(".test", { x: 500, duration: 3 });
+        gsap.to(".test", { y: -300 , duration: 3, ease: "bounce" });
     },
 };
 </script>
@@ -23,7 +22,9 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .test {
-    background-color: #42b983;
+    background-color: rgb(225, 229, 241);
+    border-radius: 20px;
+    padding: 20px;
 }
 
 h3 {
