7bc8f3d6b51564a8ddc92c820ae8ec2d0ec9b03c
seimon-m <simimueller@hotmail.com>
2021-04-01 21:22:38 +0200

    tried many different things

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-    <div v-if="setFocus">
-        Now you see me
-        <ButtonIcon @click="setFocus = false" id="forward">Back</ButtonIcon>
-        <div class="separator"/>
-        <Btn :href="'https://greensock.com/get-started/'">GSAP</Btn>
-    </div>
-    <div v-else>
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <Btn @click="setFocus = true">Focus</Btn>
-        <HelloWorld msg="Welcome to Your Vue.js App" />
-    </div>
+    <!-- <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> 
+    </div> -->
+    <router-view />
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import ButtonIcon from "./components/ButtonIcon.vue";
-import Btn from "./components/Btn.vue";
 
 export default {
     name: "App",
     components: {
-        HelloWorld,
-        ButtonIcon,
-        Btn
     },
     data() {
         return {
@@ -31,7 +18,6 @@ export default {
     },
 };
 </script>
-
 <style>
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -40,12 +26,24 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
-    
+}
+
+#nav {
+    padding: 30px;
+}
+
+#nav a {
+    font-weight: bold;
+    color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+    color: #42b983;
 }
 body {
-  background-color: #E5E5E5;
+    background-color: #e5e5e5;
 }
 .separator {
-  margin: 20px;
+    margin: 20px;
 }
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-if="setFocus">
+        Now you see me
+        <ButtonIcon @click="setFocus = false" id="forward">Back</ButtonIcon>
+        <div class="separator" />
+        <Btn :href="'https://greensock.com/get-started/'">GSAP</Btn>
+    </div>
+    <div v-else>
+        <Btn @click="setFocus = true">Focus</Btn>
+        <iframe
+            width="100%"
+            height="100%"
+            frameborder="0"
+            src="https://exorciser.ch/index"
+        />
+    </div>
+</template>
+
+<script>
+import ButtonIcon from "../components/ButtonIcon.vue";
+import Btn from "../components/Btn.vue";
+
+export default {
+    name: "About",
+    components: {
+        ButtonIcon,
+        Btn,
+    },
+    data() {
+        return {
+            setFocus: false,
+        };
+    },
+};
+</script>
