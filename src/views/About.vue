4a96f22e1c4d3b2192e17094136950f65bfd7cac
seimon-m <simimueller@hotmail.com>
2021-04-07 15:12:50 +0200

    add basic toggle button

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,17 +2,27 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <toggle-button id="switch" labelEnableText="True" labelDisableText="False" v-on:change="eventHandler"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import ToggleButton from '../components/ToggleButton.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    ToggleButton
+  },
+  methods: {
+        eventHandler(value) {
+            console.log(value);
+        }
+    }
 }
 </script>
+
+    ToggleButton
