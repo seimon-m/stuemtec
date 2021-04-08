7680339af37388bfc6ab51b3538d56fa583c8741
Simon <simimueller@hotmail.com>
2021-04-08 16:40:02 +0200

    add createWebHistory instead the hashed one

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Focus from '../views/Focus.vue'
 import Normal from '../views/Normal.vue'
@@ -30,7 +30,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
